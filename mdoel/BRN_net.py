6dbe16a8085fdd979ac8c8de403c8283b65b0a1a
wang <1277495324@qq.com>
2020-01-30 13:05:34 +0800

    complete BRN part

--- mdoel/BRN_net.py
+++ mdoel/BRN_net.py
@@ -21,25 +21,26 @@ class BRN(nn.Module):
         """
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(k + 1, 64, 3, 1),
+            nn.Conv2d(k + 1, 64, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, 3, 1),
+            nn.Conv2d(64, 64, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, 3, 1),
+            nn.Conv2d(64, 64, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(64, 128, 3, 1),
+            nn.Conv2d(64, 128, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, 3, 1),
+            nn.Conv2d(128, 128, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, 3, 1),
+            nn.Conv2d(128, 128, 3, padding=1),
             nn.ReLU(inplace=True),
         )
-        self.conv7 = nn.Conv2d(128, n*n, 3, 1)
+        self.conv7 = nn.Conv2d(128, n*n, 3, padding=1)
 
         self.conv.apply(init_weights)
         init_weights(self.conv7)
         with torch.no_grad():
             self.conv7.bias.data[12] = 1
+        self.n = n
 
     def forward(self, img, sal):
         """merge original image and saliency map from RLN output to get better saliency prediction.
@@ -50,9 +51,14 @@ class BRN(nn.Module):
         """
         img = img * 0.00392157  # normalization
         feature = torch.cat([img, sal], 1)
-        feature1 = self.conv(feature)
+        feature = self.conv(feature)
+        feature = self.conv7(feature)
 
-        return feature
+        sal = nn.functional.unfold(sal, kernel_size=self.n, padding=int((self.n-1)/2))
+        feature = feature.view((1, self.n * self.n, 384 * 384))
+        sal = sal * feature
+        sal = nn.functional.fold(sal, output_size=(384, 384), kernel_size=self.n, padding=int((self.n-1)/2))
+        return sal
 
 
 if __name__ == '__main__':
